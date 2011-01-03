2d911e86724b2f69ffcc55a360ca82f39445bc43
AshishS <AshishS@ThoughtWorks.com>
2011-01-03 19:51:27 +0530

    #Feature:0000 | File renames | <AshishS>

--- Validate.UnitTests/ValidationRepositoryFactoryTests.cs
+++ Validate.UnitTests/ValidationRepositoryFactoryTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Validate.UnitTests
 {
@@ -12,56 +11,6 @@ namespace Validate.UnitTests
             var repository1 = new ValidationRepositoryFactory().GetValidationRepository();
             var repositorty2 = new ValidationRepositoryFactory().GetValidationRepository();
             Assert.AreEqual(repository1,repositorty2);
-        }
-    }
-
-    [TestFixture]
-    public class ValidationRepositoryTests
-    {
-        [Test]
-        public void ShouldBeAbleToSaveAndGetAValidation()
-        {
-            var validation = new Validation<Person>()
-                             .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
-                             
-            var validationRepository = new ValidationRepository();
-            validationRepository.Save(validation);
-            var fetchedValidation = validationRepository.Get<Person>("Default_Validation");
-            Assert.AreEqual(validation, fetchedValidation);
-        }
-
-        [Test]
-        public void ShouldNotBeAbleToSaveMultipleValidationsForTheATypeWithTheSameName()
-        {
-            var validation1 = new Validation<Person>()
-                             .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
-            var validation2 = new Validation<Person>()
-                             .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
-
-            var validationRepository = new ValidationRepository();
-            TestDelegate testCode = () =>
-                                        {
-                                            validationRepository.Save(validation1);
-                                            validationRepository.Save(validation2);                                
-                                        };
-            Assert.Throws<ArgumentException>(testCode);
-
-        }
-
-
-        [Test]
-        public void ShouldBeAbleToRunASavedValidation()
-        {
-            var validation = new Validation<Person>()
-                            .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
-
-            var validationRepository = new ValidationRepositoryFactory().GetValidationRepository();
-            validationRepository.Save(validation);
-            
-
-            var invalidPerson = new Person();
-            var validator = invalidPerson.ValidateUsing("Default_Validation");
-            Assert.IsFalse(validator.IsValid);
         }
     }
 }
---
+++ Validate.UnitTests/ValidationRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Validate.UnitTests
+{
+    [TestFixture]
+    public class ValidationRepositoryTests
+    {
+        [Test]
+        public void ShouldBeAbleToSaveAndGetAValidation()
+        {
+            var validation = new Validation<Person>()
+                .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
+                             
+            var validationRepository = new ValidationRepository();
+            validationRepository.Save(validation);
+            var fetchedValidation = validationRepository.Get<Person>("Default_Validation");
+            Assert.AreEqual(validation, fetchedValidation);
+        }
+
+        [Test]
+        public void ShouldNotBeAbleToSaveMultipleValidationsForTheATypeWithTheSameName()
+        {
+            var validation1 = new Validation<Person>()
+                .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
+            var validation2 = new Validation<Person>()
+                .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
+
+            var validationRepository = new ValidationRepository();
+            TestDelegate testCode = () =>
+                                        {
+                                            validationRepository.Save(validation1);
+                                            validationRepository.Save(validation2);                                
+                                        };
+            Assert.Throws<ArgumentException>(testCode);
+
+        }
+
+
+        [Test]
+        public void ShouldBeAbleToRunASavedValidation()
+        {
+            var validation = new Validation<Person>()
+                .Setup(v => v.IsNotNull(p => p.Name, "Name is mandatory"));
+
+            var validationRepository = new ValidationRepositoryFactory().GetValidationRepository();
+            validationRepository.Save(validation);
+            
+
+            var invalidPerson = new Person();
+            var validator = invalidPerson.ValidateUsing("Default_Validation");
+            Assert.IsFalse(validator.IsValid);
+        }
+    }
+}
